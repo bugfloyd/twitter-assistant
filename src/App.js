27504eac7293e70c5f8dac6bb6492b23de50f3c3
Yashar Hosseinpour <yashar.hv@gmail.com>
2022-11-27 19:53:00 +0100

    Add new-tweet form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [authorName, setAuthorName] = useState("");
+  const [tweetText, setTweetText] = useState("");
 
   const tweets = [
-      {
-          text: 'This is the first tweet!',
-          author: 'Yashar',
-          date: '20-11-2022 18:50',
-          authorAvatar: '/user_avatar.png'
-      },
-      {
-          text: 'This is the second tweet!',
-          author: 'Mahsa',
-          date: '20-11-2022 17:50',
-          authorAvatar: '/user_avatar.png'
-      },
-      {
-          text: 'This is the third tweet!',
-          author: 'Pegah',
-          date: '21-11-2022 18:50',
-          authorAvatar: '/user_avatar.png'
-      }
+    {
+      text: "This is the first tweet!",
+      author: "Yashar",
+      date: "20-11-2022 18:50",
+      authorAvatar: "/user_avatar.png",
+    },
+    {
+      text: "This is the second tweet!",
+      author: "Mahsa",
+      date: "20-11-2022 17:50",
+      authorAvatar: "/user_avatar.png",
+    },
+    {
+      text: "This is the third tweet!",
+      author: "Pegah",
+      date: "21-11-2022 18:50",
+      authorAvatar: "/user_avatar.png",
+    },
   ];
-
 
   const renderTweet = (tweet) => {
     return (
-    <div className="tweet-wrap">
-      <div className='author-wrap'>
-        <img className='author-avatar' src={tweet.authorAvatar} />
-        <div className='author-name-wrap'>
-          <span>{tweet.author}</span>
+      <div className="tweet-wrap">
+        <div className="author-wrap">
+          <img className="author-avatar" src={tweet.authorAvatar} />
+          <div className="author-name-wrap">
+            <span>{tweet.author}</span>
+          </div>
+        </div>
+        <div className="tweet-text">
+          <p className="tweet">{tweet.text}</p>
+          <span>{tweet.date}</span>
         </div>
       </div>
-      <div className='tweet-text'>
-        <p className='tweet'>{tweet.text}</p>
-        <span>{tweet.date}</span>
-      </div>
-    </div>
     );
   };
 
@@ -48,12 +49,38 @@ function App() {
 
   return (
     <div>
-        <div id="header-wrap">
-            <h1>Tweet List</h1>
+      <div id="header-wrap">
+        <h1>Tweet List</h1>
+      </div>
+      <div className="add-new-wrap">
+        <div id="add-new-tweet">
+          <div id="new-tweet-author-wrap">
+            <label htmlFor="author-name">Tweet author:</label>
+            <input
+              type="text"
+              id="author-name"
+              value={authorName}
+              onChange={(event) => {
+                setAuthorName(event.target.value);
+              }}
+            />
+          </div>
+          <div id="new-tweet-text-wrap">
+            <label htmlFor="new-tweet-text">Tweet text:</label>
+            <textarea
+              id="new-tweet-text"
+              onChange={(event) => {
+                setTweetText(event.target.value);
+              }}
+            >
+              {tweetText}
+            </textarea>
+          </div>
+          <p>{tweetText}</p>
+          <button>Add</button>
         </div>
-        <div id="tweet-container">
-          {tweetsHTML}
-        </div>
+      </div>
+      <div id="tweet-container">{tweetsHTML}</div>
     </div>
   );
 }
