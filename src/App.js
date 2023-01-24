d324ac1234eb882da2868e9450a16390aae867c6
Yashar Hosseinpour <yashar.hv@gmail.com>
2023-01-24 20:14:51 +0100

    Add new tweet functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [authorName, setAuthorName] = useState("");
-  const [tweetText, setTweetText] = useState("");
-
-  const tweets = [
+  const initialTweets = [
     {
       text: "This is the first tweet!",
       author: "Yashar",
@@ -25,6 +22,11 @@ function App() {
       authorAvatar: "/user_avatar.png",
     },
   ];
+
+  const [authorName, setAuthorName] = useState("");
+  const [tweetText, setTweetText] = useState("");
+  const [tweets, setTweets] = useState(initialTweets);
+
 
   const renderTweet = (tweet) => {
     return (
@@ -47,11 +49,8 @@ function App() {
     return renderTweet(tweet);
   });
 
-  return (
-    <div>
-      <div id="header-wrap">
-        <h1>Tweet List</h1>
-      </div>
+  const addNewTweet = () => {
+    return (
       <div className="add-new-wrap">
         <div id="add-new-tweet">
           <div id="new-tweet-author-wrap">
@@ -76,10 +75,36 @@ function App() {
               {tweetText}
             </textarea>
           </div>
-          <p>{tweetText}</p>
-          <button>Add</button>
+          <button onClick={() => {
+            if (authorName && tweetText) {
+              const now = new Date();
+              const year = now.getFullYear();
+              const month = now.getMonth() + 1;
+              const day = now.getDate();
+              const hour = now.getHours();
+              const minutes = now.getMinutes();
+              setTweets([
+                ...tweets,
+                {
+                  text: tweetText,
+                  author: authorName,
+                  date: `${day}-${month}-${year} ${hour}:${minutes}`,
+                  authorAvatar: "/user_avatar.png",
+                }
+              ]);
+            }
+          }}>Add</button>
         </div>
       </div>
+    );
+  };
+
+  return (
+    <div>
+      <div id="header-wrap">
+        <h1>Tweet List</h1>
+      </div>
+      {addNewTweet()}
       <div id="tweet-container">{tweetsHTML}</div>
     </div>
   );
